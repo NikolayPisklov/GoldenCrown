2eeebcba5c114938ab2b8408a461e3929ec30337
NikolayPisklov <koluandino@gmail.com>
2026-08-01 14:48:21 +0300

    User and account service

---
+++ GoldenCrown/Services/AccountService.cs
@@ -0,0 +1,28 @@
+﻿using GoldenCrown.Database;
+using GoldenCrown.Models;
+
+namespace GoldenCrown.Services
+{
+    public interface IAccountService
+    {
+        public Task CreateAccountAsync(int userId);
+    }
+    public class AccountService : IAccountService
+    {
+        private readonly GoldenCrownDbContext _db;
+
+        public AccountService(GoldenCrownDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task CreateAccountAsync(int userId)
+        {
+            var account = new Account();
+            account.UserId = userId;
+            account.Balance = 0;
+            _db.Accounts.Add(account);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ GoldenCrown/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using GoldenCrown.Database;
+using GoldenCrown.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoldenCrown.Services
+{
+    public interface IUserService
+    {
+        public Task<bool> RegisterAsync(string login, string password, string name);
+    }
+    public class UserService : IUserService
+    {
+        private readonly GoldenCrownDbContext _db;
+        private readonly IAccountService _accountService;
+
+        public UserService(GoldenCrownDbContext db, IAccountService accountService)
+        {
+            _db = db;            
+            _accountService = accountService;
+        }
+
+        public async Task<bool> RegisterAsync(string login, string password, string name)
+        {
+            if(await _db.Users.AnyAsync(u => login == u.Login))
+            {
+                return false;
+            }
+            if(password.Length < 6) 
+            {
+                return false;
+            }
+            var user = new User();
+            user.Login = login;
+            user.Password = password;
+            user.Name = name;
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+            
+            var registeredUser = await _db.Users.FirstOrDefaultAsync(u => u.Login == login);
+            if(registeredUser is null)
+            {
+                throw new Exception("Something went wrong!");
+            }
+
+            await _accountService.CreateAccountAsync(registeredUser.Id);
+            return true;
+        }
+    }
+}
